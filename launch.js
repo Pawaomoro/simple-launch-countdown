58bce170afc333ba9375c3e20e48d20866452e60
Eddie Otieno Ratego <eddieratego@gmail.com>
2025-09-18 11:11:13 +0300

    initial commit

---
+++ launch.js
@@ -0,0 +1,22 @@
+const output = document.querySelector(".output");
+output.textContent = "";
+
+// let i = 10;
+for(let i=10; i>=0; i--){
+    const para = document.createElement('p');
+    if(i===10){
+        para.textContent = 'CountDown ${i}';
+
+    }else if(i===0){
+        para.textContent="Blast Off";
+        
+    }else{
+        para.textContent=i;
+    }
+    output.appendChild(para);
+}
+
+
+
+// 
+// output.appendChild(para);
